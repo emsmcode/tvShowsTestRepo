3011918419d0b8913eceed617d3af8b1f505af4b
Elizabeth Nickerson <elizabeth.nickerson@strongestfamilies.com>
2023-03-31 03:37:32 -0300

    restore required files and folder structure

--- cypress/e2e/specs/exercise1.cy.ts
+++ cypress/e2e/specs/exercise1.cy.ts
@@ -1,9 +1,13 @@
 /// <reference types="Cypress" />
 
+import deepEqualInAnyOrder from 'deep-equal-in-any-order'
+chai.use(deepEqualInAnyOrder)
+
 describe('TV Shows Exercise', () => {
 
+    let expectedTitles = [{title: 'The  Leftovers'}, {title: 'True Detecive'}, {title: 'Looking'}]
+
     it('1. All HBO Drama Shows that premiered AFTER 2012 and BEFORE 2016', () => {
-        let newShowList: any[] = new Array
 
         cy.request({
             method: 'GET',
@@ -13,29 +17,41 @@ describe('TV Shows Exercise', () => {
             expect(resp.status).to.eq(200)
 
         }).then((resp) => {
-            const show = resp.body
-            return show
+            const shows = resp.body
+            return shows
 
-        }).then((show) => {
-            
-            for(let i=0; i< show.length; i++) {
-                if (show[i]!.name &&
-                    show[i]!.premiered &&
-                    show[i].premiered > '2012-12-31' &&
-                    show[i].premiered < '2016-01-01' &&
-                    show[i]!.genres &&
-                    show[i].genres.includes('Drama') &&
-                    show[i]!.network &&
-                    show[i]!.network.name &&
-                    show[i].network.name === 'HBO') {
-                    
-                        newShowList.push(show[i])
+        }).then((shows) => {
+            const newShowListIDs: number[] = new Array
+            for(let i=0; i< shows.length; i++) {
+                if(
+                    shows[i]!.name &&
+                    shows[i]!.premiered &&
+                    shows[i].premiered > '2012-12-31' &&
+                    shows[i].premiered < '2016-01-01' &&
+                    shows[i]!.genres &&
+                    shows[i].genres.includes('Drama') &&
+                    shows[i]!.network &&
+                    shows[i]!.network.name &&
+                    shows[i].network.name === 'HBO') {
+
+                        newShowListIDs.push(shows[i].id)
                 }
             }
-        }).then(() => {
-            for(let i=0; i< newShowList.length; i++) {
-                cy.log(newShowList[i].name)
-           }
+            return newShowListIDs
+
+        }).then((newShowListIDs) => {
+            expect(newShowListIDs.length).to.eq(3)
+            for(let i=0; i<newShowListIDs.length; i++) {
+                cy.request({
+                    method: 'GET',
+                    url: 'shows/' + (newShowListIDs[i])
+                }).then((resp) => {
+                    expect(resp.status).to.eq(200)
+                    let theTitle = (resp.body).name
+                    cy.log(theTitle)
+                    expect(expectedTitles.includes(theTitle))
+                })
+            }
         })
     })
 })
